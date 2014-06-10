ba4410b0f3aecf91cfa5e4f8578938e695e1423d
Christian Vuerings <vueringschristian@gmail.com>
2014-06-10 11:58:49 -0700

    v0.0.1 - Get the tweet text, id, userid & timestamp

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
 var app = express();
 
 var sendError = function(res, errorMessage) {
@@ -8,16 +10,51 @@ var sendError = function(res, errorMessage) {
   });
 };
 
+var parseTweets = function(body) {
+  var $ = cheerio.load(body);
+
+  var tweets = [];
+
+  $('.js-tweet').each(function(i, element) {
+    var id = $(this).attr('data-tweet-id');
+    var time = $('.js-short-timestamp', this).attr('data-time');
+    var text = $('.js-tweet-text', this).text();
+    text = text.replace('…', '');
+    text = text.trim();
+    tweets.push({
+      createdAt: parseInt(time, 10),
+      id: id,
+      text: text
+    });
+  });
+
+  return tweets;
+};
+
+var getTweets = function(res, id) {
+  request({
+    url: 'http://twitter.com/' + id
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var tweets = parseTweets(body);
+      res.send({
+        id: id,
+        tweets: tweets
+      });
+    }
+  });
+};
+
 app.get('/:id?', function(req, res){
   var id = req.params.id;
+  var tweets = [];
 
   if (!id) {
     sendError(res, 'Please send a valid id');
+  } else {
+    tweets = getTweets(res, id);
   }
 
-  res.send({
-    'id': id
-  });
 });
 
 var port = process.env.PORT || 3000;
